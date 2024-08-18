d0116bb0601b5ffd011b2dcd0fc28fde546b79da
IT24102536 <IT24102536@my.sliit.lk>
2024-08-18 12:40:53 +0530

    Add files via upload

---
+++ Q1/IT24102536Lab4Q1.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class IT24102536Lab4Q1{
+ public static void main(String[] args){
+	 
+	 Scanner input = new Scanner(System.in);
+	 
+	 System.out.print("Enter a number: ");
+	 int num = input.nextInt();
+	 
+	 if (num > 0)
+	 {
+		 System.out.println("The number is: Positive");
+	 }
+	 else if (num < 0)
+	 {
+		 System.out.println("The number is: negative");	 
+	 }
+	 else
+         {
+		 System.out.println("The number is: zero");
+	 }
+ 
+ 
+ 
+  }
+}
+ 
+ 
+ 
+ 
